a1ec5657f99c04a7067fcb64f4058898f5b73d30
Myles Braithwaite <me@mylesbraithwaite.com>
2023-04-11 17:45:16 -0400

    👷🏼 Setup the GitHub Action to test things. (#1)

--- opendata_toronto_doors_open/service.py
+++ opendata_toronto_doors_open/service.py
@@ -13,6 +13,7 @@ def get_doorsoepn_dataset_url(package_id: str) -> str:
     response = requests.get(
         url="https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show",
         params={"id": package_id},
+        timeout=(5, 30),
     )
     response.raise_for_status()
     data = response.json()
@@ -33,7 +34,7 @@ def get_doors_open_dataset(url: str) -> List[Dict[str, Any]]:
     """
     Get the individual DoorsOpen dataset.
     """
-    response = requests.get(url)
+    response = requests.get(url, timeout=(5, 30))
     response.raise_for_status()
     return response.json()
 
